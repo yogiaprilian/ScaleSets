98889f47510d522d71afc7b5f3d8be9b62f41238
yogiaprilian <45745493+yogiaprilian@users.noreply.github.com>
2019-02-11 11:43:03 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,1 @@
-sudo mkdir /home/yogi/testttttt
-
-sudo mkdir /home/yogi/tesupdate
-
-sudo mkdir /home/yogi/testfolder3
-
-sudo mkdir /home/yogi/folderbaru
-
-sudo mkdir /home/yogi/folderjumat
-
-sudo mkdir /home/yogi/folderdata
-
-sudo mkdir /home/yogi/folderdata2
+sudo mkdir /home/yogi/folderscalesets
